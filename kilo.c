dabbfdd646b0af94aa91a42f9b7471f81789f153
AlbertHeurlindeOliveira <abbbe009@gmail.com>
2024-10-19 16:54:11 +0200

    Error handling added

--- kilo.c
+++ kilo.c
@@ -1,20 +1,33 @@
+/*** includes ***/
+
 #include <unistd.h> //Standard symbolic constants and types
 #include <termios.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <errno.h>
+
+/*** data ***/
 
 struct termios orig_termios; //To keep track of initial terminal attributes
 
+/*** terminal ***/
+
+void die(const char* s){
+    perror(s); //looks at global errno variable and prints description for it
+    exit(1);
+}
+
 void disableRawMode(){
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); //Resets terminal attributes
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) //Resets terminal attributes
+        die("tcsetattr: Error on resetting terminal attributes\r\n");
 }
 
 //Function to enable us to process inputs without them being submitted with 'Enter'
 //Also stores initial terminal attributes
 void enableRawMode(){
 
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr: Error on reading terminal attributes\r\n");
     atexit(disableRawMode); //Call disableRawMode automatically when program exits
 
     struct termios raw = orig_termios;
@@ -27,25 +40,29 @@ void enableRawMode(){
     raw.c_cflag |= ~(CS8); //Sets character size to 8 bits per byte
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
-    raw.c_cc[VMIN] = 0;
-    raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_cc[VMIN] = 0; //Sets minimum number of bytes before read() can return
+    raw.c_cc[VTIME] = 1; //How fast read() times out in tenth of seconds
+
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr: Error on setting raw struct\r\n");
 }
+
+/*** init ***/
 
 int main(){
     enableRawMode();
 
-    char c;
     //read returns 0 at E0F
     while (1){ //Reads from FILENO to c, 1 byte
-
+        char c = '\0';
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read\r\n");
         //Prints character and and ASCII value if not control character
         if(iscntrl(c)){
             printf("%d\r\n", c);
         } else {
             printf("%d ('%c')\r\n", c, c);
         }
+        if(c == 'q') break;
     }
     return 0;
 }
